a8435d69e60319180c358a113e5b2915a6aee187
peymn88gh <peymn88gh@gmail.com>
2023-01-06 04:13:54 +0330

    creating createNote page

--- material-ui-tut-lesson-2/src/App.js
+++ material-ui-tut-lesson-2/src/App.js
@@ -1,19 +1,31 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import {createTheme} from "@material-ui/core";
+import {ThemeProvider} from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ce93d8",
+    },
+  },
+});
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- material-ui-tut-lesson-2/src/pages/Create.js
+++ material-ui-tut-lesson-2/src/pages/Create.js
@@ -1,31 +1,109 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { Button, TextField } from "@material-ui/core";
+import { Container } from "@material-ui/core";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core";
+import { Block } from "@mui/icons-material";
+import { useState } from "react";
+import { FormControl, FormLabel } from "@material-ui/core";
+import { Radio } from "@material-ui/core";
+import { RadioGroup } from "@material-ui/core";
+import { FormControlLabel } from "@material-ui/core";
+import { flexbox } from "@mui/system";
 
+const useStyles = makeStyles({
+  form: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "flex",
+    flexDirection: "column",
+    justifyContnet: "center",
+    alignItems: "flex-start",
+  },
+  fields: {
+    marginTop: 5,
+    marginBottom: 5,
+  },
+});
 export default function Create() {
+  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [validTitle, setvalidTitle] = useState(true);
+  const [validDetails, setvalidDetails] = useState(true);
+  const [category, setCategory] = useState("todos");
+  const hanldeSubmit = () => {
+    fetch("http://localhost:8000/notes", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ title, details, category }),
+    });
+  };
+  const handleChange = (e) => {
+    setCategory(e.target.value);
+  };
   return (
-    <div>
+    <Container>
       <Typography
-        variant="h6" 
+        variant="h6"
         color="textSecondary"
         component="h2"
         gutterBottom
       >
         Create a New Note
       </Typography>
-
-      {/* <Typography
-        variant="h1" 
-        color="primary"
-        align="center"
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={hanldeSubmit}
       >
-        Create a New Note
-      </Typography>
-      <Typography
-        noWrap
-        color="secondary"
-      >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque molestiae magnam voluptate reiciendis fuga totam facere officia, animi sint laboriosam dolorum voluptas cupiditate explicabo dolore veritatis recusandae rem inventore sunt.
-      </Typography> */}
-    </div>
-  )
+        <TextField
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          className={classes.fields}
+          variant="outlined"
+          label="Note Title"
+          fullWidth
+          required
+        />
+        <TextField
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
+          className={classes.fields}
+          variant="outlined"
+          label="Details"
+          fullWidth
+          required
+          multiline
+          minRows={4}
+        />
+        <FormControl className={classes.form}>
+          <FormLabel>choose category</FormLabel>
+          <RadioGroup value={category} onChange={handleChange}>
+            <FormControlLabel value="money" control={<Radio />} label="money" />
+            <FormControlLabel control={<Radio />} value="time" label="time" />
+            <FormControlLabel control={<Radio />} value="todos" label="todos" />
+            <FormControlLabel
+              control={<Radio />}
+              value="travel"
+              label="travel"
+            />
+          </RadioGroup>
+        </FormControl>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          endIcon={<KeyboardArrowRightIcon />}
+          style={{ marginTop: 10 }}
+        >
+          submit
+        </Button>
+      </form>
+    </Container>
+  );
 }
